54984221587cfc672638562e291f42520e053c80
Steven J. Gomez Alvarado <stevenjgomez@ucsb.edu>
2025-06-19 14:08:39 -0500

    Allow handling of Path objects

--- src/nxrefine/nxos.py
+++ src/nxrefine/nxos.py
@@ -1,27 +1,36 @@
 import re
 import platform
 
+from pathlib import Path
+
 def is_wsl():
     """Return True if operating in Windows Subsystem for Linux (WSL)"""
     return "WSL" in platform.uname().release
 
 def to_posix(path):
     """Converts absolute Windows path to absolute POSIX path."""
-    path = path.replace('\\','/')
-    match = re.match(r"^([A-Za-z]):/(.*)", path)
+    str_path = str(path).replace('\\','/')
+    match = re.match(r"^([A-Za-z]):/(.*)", str_path)
     if match:
         drive_letter = match.group(1).lower()
         rest = match.group(2)
-        return f"/mnt/{drive_letter}/{rest}"
+        formatted_path = f"/mnt/{drive_letter}/{rest}"
+        if isinstance(path, Path):
+            return Path(formatted_path)
+        return formatted_path
     
     raise ValueError("Invalid absolute path.")
 
 def to_windows(path):
     """Converts absolute POSIX path to absolute Windows path."""
-    match = re.match(r"^/mnt/([a-z])/([^:]*)", path)
+    str_path = str(path)
+    match = re.match(r"^/mnt/([a-z])/([^:]*)", str_path)
     if match:
         drive_letter = match.group(1).upper()
         rest = match.group(2).replace('/','\\')
-        return f"{drive_letter}:\\{rest}"
+        formatted_path = f"{drive_letter}:\\{rest}"
+        if isinstance(path, Path):
+            return Path(formatted_path)
+        return formatted_path
     
     raise ValueError("Invalid absolute path.")
