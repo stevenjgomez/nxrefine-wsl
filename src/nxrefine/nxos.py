60f5d92778f2807af300aedbf8bc9cabe8b11736
Steven J. Gomez Alvarado <stevenjgomez@ucsb.edu>
2025-06-19 14:40:58 -0500

    Always return string

--- src/nxrefine/nxos.py
+++ src/nxrefine/nxos.py
@@ -1,8 +1,6 @@
 import re
 import platform
 
-from pathlib import Path
-
 def is_wsl():
     """Return True if operating in Windows Subsystem for Linux (WSL)"""
     return "WSL" in platform.uname().release
@@ -15,22 +13,18 @@ def to_posix(path):
         drive_letter = match.group(1).lower()
         rest = match.group(2)
         formatted_path = f"/mnt/{drive_letter}/{rest}"
-        if isinstance(path, Path):
-            return Path(formatted_path)
         return formatted_path
     
     raise ValueError("Invalid absolute path.")
 
 def to_windows(path):
     """Converts absolute POSIX path to absolute Windows path."""
-    str_path = str(path)
+    str_path = str(path).replace('\\','/')
     match = re.match(r"^/mnt/([a-z])/([^:]*)", str_path)
     if match:
         drive_letter = match.group(1).upper()
         rest = match.group(2).replace('/','\\')
         formatted_path = f"{drive_letter}:\\{rest}"
-        if isinstance(path, Path):
-            return Path(formatted_path)
         return formatted_path
     
     raise ValueError("Invalid absolute path.")
